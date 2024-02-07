9f245592deb8da2720e97c4c67dcabd04a2c716a
habipakyol <20181102066@ogr.msgsu.edu.tr>
2024-02-08 02:36:51 +0300

    no message

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,12 @@
+﻿using Business.Concrete;
+using Entities.Concretes;
+
+Person person = new();
+
+person.FirstName = "Habip";
+person.LastName = "Akyol";
+person.DateOfBirthYear = 1999;
+person.NationalIdentity = 11223344556;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
